16d5b7f94c02ea60b3dd5d190657c07404eb888b
i5ting <i5ting@126.com>
2016-07-25 23:43:25 +0800

    refact

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ var path = require('path')
 var debug = require('debug')('jwf')
 
 module.exports = function (file) {
-  var obj = require(path.join(__dirname, file))
-  
-  return parse (obj) ;
+  var _path = path.join(__dirname, file)
+  var obj = require(_path)
+  return parse(obj)
 }
 
 function parse (obj) {
   var result = {}
-  for(var i in obj){
-    debug('result[i] ' + i  + ' - ' + obj[i] )
-    if (typeof obj[i] == 'object') {
+  for (var i in obj) {
+    debug('result[i] ' + i + ' - ' + obj[i])
+    if (typeof obj[i] === 'object') {
       debug(obj[i])
-      debug(parse (obj[i]))
-      result[i] = parse (obj[i])
+      debug(parse(obj[i]))
+      result[i] = parse(obj[i])
       debug(result)
       // break
-    }else if (fs.existsSync(obj[i]) === true) {
-      debug('existsSync ' + i  + ' - ' + obj[i] )
+    } else if (fs.existsSync(obj[i]) === true) {
+      debug('existsSync ' + i + ' - ' + obj[i])
       result[i] = require(path.join(__dirname, obj[i]))
       debug(result[i])
     } else {
       result[i] = obj[i]
     }
   }
-  
+
   return result
 }
